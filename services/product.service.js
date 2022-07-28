c988e5427683824c84b5d7ae36b78f90282ef141
André Izarra <programador385@gmail.com>
2022-07-28 15:42:24 -0400

    product aggregate

--- services/product.service.js
+++ services/product.service.js
@@ -2,8 +2,56 @@ const ProductModel = require("../database/models/product.model");
 
 class ProductService {
   async getProducts() {
-    const products = await ProductModel.find({});
-    return products;
+    const productsAggregate = await ProductModel.aggregate([
+      {
+        $match: { isActive: true },
+      },
+      {
+        $lookup: {
+          from: "productvariations",
+          localField: "variations",
+          foreignField: "_id",
+          as: "variations",
+        },
+      },
+      {
+        $lookup: {
+          from: "brands",
+          localField: "brand",
+          foreignField: "_id",
+          as: "brand",
+        },
+      },
+      {
+        $lookup: {
+          from: "categories",
+          localField: "category",
+          foreignField: "_id",
+          as: "category",
+        },
+      },
+      {
+        $unset: [
+          "createdAt",
+          "updatedAt",
+          "__v",
+          "isActive",
+          "variations.createdAt",
+          "variations.updatedAt",
+          "variations.__v",
+          "variations.isActive",
+          "brand.createdAt",
+          "brand.updatedAt",
+          "brand.__v",
+          "brand.isActive",
+          "category.__v",
+          "category.createdAt",
+          "category.updatedAt",
+          "category.isActive",
+        ],
+      },
+    ]);
+    return productsAggregate;
   }
 
   async createProduct(body) {
@@ -12,6 +60,15 @@ class ProductService {
   }
 
   async updateProduct(id, body) {}
+
+  async addVariation(id, variationId) {
+    const product = await ProductModel.findById({ _id: id });
+    await ProductModel.findByIdAndUpdate(
+      id,
+      { variations: product.variations.concat(variationId) },
+      { new: true }
+    );
+  }
 
   async deleteProduct(id) {
     const product = await ProductModel.findByIdAndUpdate(
