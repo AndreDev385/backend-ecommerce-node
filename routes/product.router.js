86a846f118ce91367e945321cf0709d6da14ab8e
Andresargote <andresargote2019@gmail.com>
2022-08-31 21:18:36 +0200

    Fix product dashboard endpoint

--- routes/product.router.js
+++ routes/product.router.js
@@ -1,121 +1,123 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
-const ProductService = require("../services/product.service");
-const VariationService = require("../services/productVariation.service");
-const validatorHandler = require("../middlewares/validator.handler");
-const { checkJWT, isRole } = require("../middlewares/auth.handler");
+const ProductService = require('../services/product.service');
+const VariationService = require('../services/productVariation.service');
+const validatorHandler = require('../middlewares/validator.handler');
+const { checkJWT, isRole } = require('../middlewares/auth.handler');
 const {
-	createProductSchema,
-	idSchema,
-	updateProductSchema,
-} = require("../schemas/product.schema");
+  createProductSchema,
+  idSchema,
+  updateProductSchema,
+} = require('../schemas/product.schema');
 
 const productService = new ProductService();
 const variationService = new VariationService();
 
-router.get("/", async (req, res, next) => {
-	try {
-		const products = await productService.getProducts();
-		res.status(200).json({ message: "success", body: products });
-	} catch (error) {
-		next(error);
-	}
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await productService.getProducts();
+    res.status(200).json({ message: 'success', body: products });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/:id", async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const product = await productService.retrieveProduct(id);
-		res.status(200).json({ message: "success", body: product });
-	} catch (error) {
-		next(error);
-	}
+router.get('/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const product = await productService.retrieveProduct(id);
+    res.status(200).json({ message: 'success', body: product });
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post(
-	"/",
-	checkJWT,
-	isRole("admin", "seller"),
-	validatorHandler(createProductSchema, "body"),
-	async (req, res, next) => {
-		try {
-			const { body } = req;
-			const newProduct = await productService.createProduct(body);
-			res.status(201).json({
-				message: "Product has been created",
-				body: newProduct,
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
+  '/',
+  checkJWT,
+  isRole('admin', 'seller'),
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { body } = req;
+      const newProduct = await productService.createProduct(body);
+      res.status(201).json({
+        message: 'Product has been created',
+        body: newProduct,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
-router.post("/dashboard", async (req, res, next) => {
-	try {
-		const { body } = req;
-		const product = await productService.createProduct({
-			name: body.name,
-			brand: body.brand,
-			category: body.category,
-			images: body.images,
-			description: body.description ? body.description : "",
-			tags: body.tags ? body.tags : [],
-		});
-		body.variations.map(
-			async ({ normalPrice, offerPrice, stock, attributes }) => {
-				const variation = await variationService.createVariation({
-					product: product.id,
-					normalPrice,
-					offerPrice,
-					stock,
-					attributes,
-				});
-				await productService.addVariation(product.id, variation.id);
-			}
-		);
-		const result = await productService.retrieveProduct(product.id);
-		res.json(result);
-	} catch (error) {
-		next(error);
-	}
+router.post('/dashboard', async (req, res, next) => {
+  try {
+    const variationsIds = [];
+
+    const { body } = req;
+    const product = await productService.createProduct({
+      name: body.name,
+      brand: body.brand,
+      category: body.category,
+      images: body.images,
+      description: body.description ? body.description : '',
+      tags: body.tags ? body.tags : [],
+    });
+
+    for (const variation of body.variations) {
+      const newVariation = await variationService.createVariation({
+        product: product._id,
+        ...variation,
+      });
+
+      variationsIds.push(newVariation._id);
+    }
+
+    await productService.addVariations(product._id, variationsIds);
+
+    const result = await productService.retrieveProduct(product.id);
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.put(
-	"/:id",
-	checkJWT,
-	isRole("admin", "seller"),
-	validatorHandler(idSchema, "params"),
-	validatorHandler(updateProductSchema, "body"),
-	async (req, res, next) => {
-		try {
-			const { id } = req.params;
-			const { body } = req;
-			const updatedProduct = await productService.updateProduct(id, body);
-			res.json({ message: "updated", body: updatedProduct });
-		} catch (error) {
-			next(error);
-		}
-	}
+  '/:id',
+  checkJWT,
+  isRole('admin', 'seller'),
+  validatorHandler(idSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const { body } = req;
+      const updatedProduct = await productService.updateProduct(id, body);
+      res.json({ message: 'updated', body: updatedProduct });
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
 router.delete(
-	"/:id",
-	checkJWT,
-	isRole("admin", "seller"),
-	validatorHandler(idSchema, "params"),
-	async (req, res, next) => {
-		try {
-			const { id } = req.params;
-			const deletedProduct = await productService.deleteProduct(id);
-			res.status(204).json({
-				message: "succesfully deleted",
-				data: deletedProduct,
-			});
-		} catch (error) {
-			next(error);
-		}
-	}
+  '/:id',
+  checkJWT,
+  isRole('admin', 'seller'),
+  validatorHandler(idSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const deletedProduct = await productService.deleteProduct(id);
+      res.status(204).json({
+        message: 'succesfully deleted',
+        data: deletedProduct,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
 );
 
 module.exports = router;
--- services/product.service.js
+++ services/product.service.js
@@ -1,61 +1,68 @@
-const boom = require("@hapi/boom");
+const boom = require('@hapi/boom');
 
-const ProductModel = require("../database/models/product.model");
+const ProductModel = require('../database/models/product.model');
 const {
-	productListAggregate,
-	productRetrieveAggregate,
-} = require("./utils/productService.query");
+  productListAggregate,
+  productRetrieveAggregate,
+} = require('./utils/productService.query');
 
 class ProductService {
-	async getProducts() {
-		const productsAggregate = await ProductModel.aggregate(
-			productListAggregate
-		);
-		return productsAggregate;
-	}
+  async getProducts() {
+    const productsAggregate = await ProductModel.aggregate(productListAggregate);
+    return productsAggregate;
+  }
 
-	async createProduct(body) {
-		const product = await ProductModel.create(body);
-		return product;
-	}
+  async createProduct(body) {
+    const product = await ProductModel.create(body);
+    return product;
+  }
 
-	async productExists(id) {
-		const product = await ProductModel.findOne({ _id: id, isActive: true });
-		if (!product) throw boom.badRequest("Product Not found");
-	}
+  async productExists(id) {
+    const product = await ProductModel.findOne({ _id: id, isActive: true });
+    if (!product) throw boom.badRequest('Product Not found');
+  }
 
-	async retrieveProduct(id) {
-		await this.productExists(id);
-		const product = await ProductModel.aggregate(productRetrieveAggregate(id));
-		return product;
-	}
+  async retrieveProduct(id) {
+    await this.productExists(id);
+    const product = await ProductModel.aggregate(productRetrieveAggregate(id));
+    return product;
+  }
 
-	async updateProduct(id, body) {
-		await this.productExists(id);
-		const product = await ProductModel.findByIdAndUpdate(id, body, {
-			new: true,
-		});
-		return product;
-	}
+  async updateProduct(id, body) {
+    await this.productExists(id);
+    const product = await ProductModel.findByIdAndUpdate(id, body, {
+      new: true,
+    });
+    return product;
+  }
 
-	async addVariation(productId, variationId) {
-		const product = await ProductModel.findById({ _id: productId });
-		await ProductModel.findByIdAndUpdate(
-			productId,
-			{ variations: product.variations.concat(variationId) },
-			{ new: true }
-		);
-	}
+  async addVariation(productId, variationId) {
+    const product = await ProductModel.findById({ _id: productId });
+    await ProductModel.findByIdAndUpdate(
+      productId,
+      { variations: product.variations.concat(variationId) },
+      { new: true }
+    );
+  }
 
-	async deleteProduct(id) {
-		await this.productExists(id);
-		const product = await ProductModel.findByIdAndUpdate(
-			{ _id: id },
-			{ isActive: false },
-			{ new: true }
-		);
-		return product;
-	}
+  async addVariations(productId, variationsIds) {
+    const product = await ProductModel.findById({ _id: productId });
+    await ProductModel.findByIdAndUpdate(
+      productId,
+      { variations: variationsIds },
+      { new: true }
+    );
+  }
+
+  async deleteProduct(id) {
+    await this.productExists(id);
+    const product = await ProductModel.findByIdAndUpdate(
+      { _id: id },
+      { isActive: false },
+      { new: true }
+    );
+    return product;
+  }
 }
 
 module.exports = ProductService;
